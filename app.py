3005abca240ecb3962a7c8aa1e8c794031fe7e0f
MustafaEht <107899109+MustafaEht@users.noreply.github.com>
2023-07-14 13:07:20 -0400

    Changes to App.py

--- app.py
+++ app.py
@@ -1,71 +1,64 @@
-# 1. Library imports
-import uvicorn
-from fastapi import FastAPI
-import numpy as np
-import pickle
-import pandas as pd
-from fastapi import FastAPI, File, UploadFile,Form
-from pydantic import BaseModel
+from flask import Flask, jsonify, request
 from keras.models import load_model
 from keras.utils import load_img
+#from keras.utils.image import img_to_array
+import numpy as np
+from flask_cors import CORS
 import cv2
-from fastapi.middleware.cors import CORSMiddleware
 
+app = Flask(__name__)
+CORS(app, resources={r"/*": {"origins": ["http://localhost",
+                                          "http://localhost:8080",
+                                          "https://myapp.herokuapp.com",
+                                          "http://www.e-hospital.ca/Skincancer",
+                                          "http://www.e-hospital.ca",
+                                          "http://localhost:5000"]}})
 
-# 2. Create the app object
-app = FastAPI()
-pickle_in = open("classifier.pkl","rb")
-#classifier=pickle.load(pickle_in)
-classifier=load_model('my_model.hdf5')
+model = load_model('my_model.hdf5')
 
-origins = ["http://localhost", "http://localhost:8080", "https://myapp.herokuapp.com","http://www.e-hospital.ca/","http://www.e-hospital.ca","http://localhost:5000"]
+model.make_predict_function()
 
-app.add_middleware(
-    CORSMIDDLEware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"]
-    allow_headers=["*"],
-)
-
-# 3. Index route, opens automatically on http://127.0.0.1:8000
-@app.get('/')
-def index():
-    return {'message': 'Skin Cancer Detector'}
-
-# 4. Route with a single parameter, returns the parameter within a message
-#    Located at: http://127.0.0.1:8000/AnyNameHere
-@app.get('/{name}')
-def hello_name(name: str):
-    return {'message': f'Our project {name}'}
-
-# 3. Expose the prediction functionality, make a prediction from the passed
-#    JSON data and return the predicted Bank Note with the confidence
-@app.post('/predict')
-def predict_skinCancer(file: UploadFile = File(...)):
-   
-    image = load_img(file.filename)
-    image = np.asarray(image)
-    img = image
-    img_resize = (cv2.resize(img, dsize=(244, 244),    interpolation=cv2.INTER_CUBIC))/255.
+def predict_label(img_path):
+	i = load_img(img_path, target_size=(244,244))
+	image = np.asarray(i)
+	img = image
+	img_resize = (cv2.resize(img, dsize=(244, 244),    interpolation=cv2.INTER_CUBIC))/255.
         
-    img_reshape = img_resize[np.newaxis,...]
+	img_reshape = img_resize[np.newaxis,...]
  
-    
-    prediction = classifier.predict(img_reshape)
-    if(prediction[0]>0):
-        prediction="Cancer"
-        print(prediction)
-    else:
-        prediction="Not Cancer"
-        print(prediction)
-    return {
-        'prediction': prediction
-    }
+	#p = model.predict(i)
+	
+	classes_x=np.argmax(p,axis=1)
+	if model.predict(img_reshape) < 0:
+		return "Not Cancer "
+	elif model.predict(img_reshape) > 0:
+		return "Cancer"
+	else:
+		return "Incorrect Image"
 
-# 5. Run the API with uvicorn
-#    Will run on http://127.0.0.1:8000
-if __name__ == '__main__':
-    uvicorn.run(app, host='127.0.0.1', port=8000)
+@app.route("/", methods=['GET'])
+def main():
+	return {'message': 'Welcome to our Project!'}
+
+
+@app.route('/upload', methods=['POST'])
+def upload_file():
+    # Check if the POST request has a file part.
+    if 'file' not in request.files:
+        return jsonify({'error': 'No file part in the request'}), 400
     
-#uvicorn app:app --reload
+    file = request.files['file']
+    
+    # If the user does not select a file, the browser submits an empty file.
+    if file.filename == '':
+        return jsonify({'error': 'No file selected'}), 400
+    
+    # Save the file to disk.
+    file.save(file.filename)
+    p = predict_label(file.filename)
+
+    return jsonify({'message': p}), 200
+
+
+if __name__ == '__main__':
+    app.run(threaded=True, port=5000)
